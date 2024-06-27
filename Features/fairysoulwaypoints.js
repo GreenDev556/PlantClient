14a4931a02a1a1f17b87b9db236f5e02a7359bd0
x45k <michaelbrennom@gmail.com>
2024-06-27 09:22:19 -0500

    added fairy soul shit cause green is lazy

---
+++ Features/fairysoulwaypoints.js
@@ -0,0 +1,19 @@
+import Config from "../Config"
+import { createWaypoint } from "../utils/utils"
+import { getEntitySkullTexture, EntityArmorStand} from "../../BloomCore/utils/Utils"
+
+const soulString = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjk2OTIzYWQyNDczMTAwMDdmNmFlNWQzMjZkODQ3YWQ1Mzg2NGNmMTZjMzU2NWExODFkYzhlNmIyMGJlMjM4NyJ9fX0="
+
+register("renderWorld", () => {
+    World.getAllEntitiesOfType(EntityArmorStand).forEach(Entity => {
+        let posx = Entity.getX()
+        let posy = Entity.getY()
+        let posz = Entity.getZ()
+        if (getEntitySkullTexture(Entity) === null) return
+        if (getEntitySkullTexture(Entity) !== soulString) return
+        if (!Config.showSouls) return
+        createWaypoint("any", "Fairy Soul", 128, 0, 128, posx, posy, posz)
+    })
+
+
+})
--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,5 @@
+import renderBeaconBeam from "../../BeaconBeam";
+
 export function getWorld() {
     let area = 'null'
     try {
@@ -14,4 +16,11 @@ export function isInDungeon() {
     try {
         return TabList?.getNames()?.some(a => a.removeFormatting() == 'Dungeon: Catacombs')
     } catch (e) { }
+}
+
+export function createWaypoint(world, text, redval, greenval, blueval, posx, posy, posz) {
+    if (getWorld() == world || world == "any") {
+        renderBeaconBeam(posx, posy, posz, redval, greenval, blueval, 0.5, true, 300)
+        Tessellator.drawString(text, posx, posy+1, posz, -1157562624, true, 1, true)
+    }
 }
